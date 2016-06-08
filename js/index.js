061c81f9100c36cfa1a1ca8f63cf992c7adaefa1
dwalsh289 <dwalsh289@gmail.com>
2016-06-08 17:35:37 -0400

    finished skeleton, just requires finishing touches.

--- js/index.js
+++ js/index.js
@@ -11,5 +11,13 @@ $(function(){
     });
 
 
+    var jumboHeight = $('.jumbotron').outerHeight();
+    function parallax(){
+        var scrolled = $(window).scrollTop();
+        $('.bg').css('height', (jumboHeight-scrolled) + 'px');
+    }
 
+    $(window).scroll(function(e){
+        parallax();
+    });
 });
